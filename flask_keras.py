8a16e72c76285d8fc848487b5274733096e4711c
yuelingxi <ylingxi@qq.com>
2019-05-26 19:18:51 +0800

    增加flask模块

---
+++ flask_keras.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+from predicate import model
+from flask import Flask,jsonify,request
+import jieba
+
+app = Flask(__name__)
+@app.route('/predict', methods=["POST"])
+def predict():
+    params = request.form.get('question')
+
+    # 若发现参数，则返回预测值
+    if (params != None):
+        quesionToken=questionPreProcessor(params)
+        tag = model.predicated_quick(quesionToken)
+        print(tag)
+        my_dict = {"entity": "", " predicate": tag}
+    # 返回响应
+    return jsonify(my_dict)
+
+
+# 當啟動 server 時先去預先 load model 每次 request 都要重新 load 造成效率低下且資源浪費
+if __name__ == '__main__':
+     app.run(debug=False, host='0.0.0.0', port=6006)
+def questionPreProcessor(q):
+    str = " ";
+
+    question_list = jieba.cut(q)
+
+    quesionToken=str.join( question_list )
+    return quesionToken
